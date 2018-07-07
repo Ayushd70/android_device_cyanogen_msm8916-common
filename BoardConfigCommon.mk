944c8ecb7f6eaa5c70a6ae5e5a236a7de569e141
Ayush Dubey <ayushdubey70@gmail.com>
2018-07-07 10:05:53 +0000

    msm8916:RIP sepolicy
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -129,8 +129,8 @@ MALLOC_SVELTE := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
+#include device/qcom/sepolicy/sepolicy.mk
+#BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
