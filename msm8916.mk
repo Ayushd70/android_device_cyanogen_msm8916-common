73994e7fd1a056be3bedb3ec759c9328ffd0b9c6
Ayush Dubey <ayushdubey70@gmail.com>
2018-07-17 08:50:24 +0000

    msm8916:DU changes DNM_WIP
    Signed-off-by: Ayush Dubey <ayushdubey70@gmail.com>

--- msm8916.mk
+++ msm8916.mk
@@ -88,6 +88,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl \
     android.hardware.gnss@1.0-service
+# Health HAL
+PRODUCT_PACKAGES += \
+    android.hardware.health@1.0-impl \
+    android.hardware.health@1.0-convert \
+    android.hardware.health@1.0-service \
+    android.hardware.health@1.0
 
 # Init scripts
 PRODUCT_PACKAGES += \
